1b6a2c312394dd04734c43344cec0aaf25fa2960
YanaShel <yana.shelementseva@gmail.com>
2020-02-03 10:43:10 +0200

    Effect for image

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ var pictures = createPictures(25);
 renderPictures(pictures);
 
 var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+// bigPicture.classList.remove('hidden');
 
 var viewingBigPhoto = function (picture) {
   var pictureImg = bigPicture.querySelector('.big-picture__img img');
@@ -103,6 +103,7 @@ var viewingBigPhoto = function (picture) {
   likesCount.textContent = picture.likes;
   commentsCount.textContent = picture.comments.length;
   descriptionPhoto.textContent = picture.description;
+  renderComments(picture);
 };
 
 var commentsBlock = bigPicture.querySelector('.social__comments');
@@ -129,9 +130,116 @@ var renderComments = function (picture) {
 };
 
 viewingBigPhoto(pictures[0]);
-renderComments(pictures[0]);
 
 bigPicture.querySelector('.social__comment-count').classList.add('hidden');
 bigPicture.querySelector('.comments-loader').classList.add('hidden');
+
+// Показ формы редактирования
+var upLoadFileInput = document.querySelector('#upload-file');
+var imageEditor = document.querySelector('.img-upload__overlay');
+var closeImageEditor = document.querySelector('#upload-cancel');
+
+var onImgEditorEscPress = function (evt) {
+  if (evt.key === 'Escape') {
+    imageEditor.classList.add('hidden');
+  }
+};
+
+upLoadFileInput.addEventListener('change', function () {
+  imageEditor.classList.remove('hidden');
+  document.addEventListener('keydown', onImgEditorEscPress);
+});
+
+closeImageEditor.addEventListener('click', function () {
+  imageEditor.classList.add('hidden');
+  document.removeEventListener('keydown', onImgEditorEscPress);
+});
+
+// Применение эффекта для изображения
+var slider = document.querySelector('.effect-level');
+var sliderLine = slider.querySelector('.effect-level__line');
+var sliderPin = sliderLine.querySelector('.effect-level__pin');
+var sliderDepthColor = sliderLine.querySelector('.effect-level__depth');
+var effectLevelInput = slider.querySelector('.effect-level__value');
+var imgPreview = document.querySelector('.img-upload__preview img');
+
+var onChangeEffectImg = function () {
+  var effectLevel = getEffectLevel();
+  sliderDepthColor.style.width = effectLevel + '%';
+  effectLevelInput.value = effectLevel;
+};
+
+sliderPin.addEventListener('mouseup', onChangeEffectImg);
+
+var effectList = document.querySelector('.effects__list');
+var chrome = effectList.querySelector('#effect-chrome');
+var heat = effectList.querySelector('#effect-heat');
+var sepia = effectList.querySelector('#effect-sepia');
+var marvin = effectList.querySelector('#effect-marvin');
+var phobos = effectList.querySelector('#effect-phobos');
+var original = effectList.querySelector('#effect-none');
+
+original.addEventListener('click', function (evt) {
+  if (evt.target.value === 'none') {
+    imgPreview.style.filter = 'none';
+    // slider.classList.add('hidden');
+  }
+});
+
+chrome.addEventListener('click', function (evt) {
+  if (evt.target.value === 'chrome') {
+    imgPreview.style.filter = 'grayscale(' + grayscaleEffect() + ')';
+  }
+});
+
+sepia.addEventListener('click', function (evt) {
+  if (evt.target.value === 'sepia') {
+    imgPreview.style.filter = 'sepia(' + sepiaEffect() + ')';
+  }
+});
+
+marvin.addEventListener('click', function (evt) {
+  if (evt.target.value === 'marvin') {
+    imgPreview.style.filter = 'invert(' + getEffectLevel() + '%)';
+  }
+});
+
+phobos.addEventListener('click', function (evt) {
+  if (evt.target.value === 'phobos') {
+    imgPreview.style.filter = 'blur(' + phobosEffect() + 'px)';
+  }
+});
+
+heat.addEventListener('click', function (evt) {
+  if (evt.target.value === 'heat') {
+    imgPreview.style.filter = 'brightness(' + brightness() + ')';
+  }
+});
+
+// Для эффекта «Хром» — filter: grayscale(0..1)
+var grayscaleEffect = function () {
+  return getEffectLevel() / 100;
+};
+
+// Для эффекта «Сепия» — filter: sepia(0..1)
+var sepiaEffect = function () {
+  return getEffectLevel() / 100;
+};
+
+// Для эффекта «Фобос» — filter: blur(0..3px)
+var phobosEffect = function () {
+  return 3 / 100 * getEffectLevel();
+};
+
+// Для эффекта «Зной» — filter: brightness(1..3)
+var brightness = function () {
+  return (2 / 100 * getEffectLevel()) + 1;
+};
+
+var getEffectLevel = function () {
+  var widthLine = sliderLine.clientWidth;
+  var sliderPinLeft = sliderPin.offsetLeft;
+  return Math.floor((sliderPinLeft * 100) / widthLine);
+};
 
 
